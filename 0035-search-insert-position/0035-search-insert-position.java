1b2b3d36644d3cbdeb91d8a3bffb47be7fc2c6a7
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-07-07 16:38:16 +0530

    Time: 0 ms (100%), Space: 44.7 MB (66.21%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,5 +1,14 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        /*
+        NOTE:
+        For insertion-type binary search:
+        If you find the target, return mid.
+        If you don't find it, the loop ends with s > e.
+        At that moment:
+        s is the first index where the target can be inserted.
+        e is the last index with a value smaller than the target.
+        */
         int n = nums.length;
         int s = 0;
         int e = n - 1;
