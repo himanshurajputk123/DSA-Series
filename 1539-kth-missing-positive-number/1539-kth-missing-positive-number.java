1c4e0d0e49750e20ba48dd82d74430f492db6d79
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-07-14 18:01:28 +0530

    Time: 0 ms (100%), Space: 44.6 MB (39.75%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -4,10 +4,28 @@ class Solution {
 
         // BruteForce
             // O(n);
-        for(int i = 0; i<n; i++){
-            if(arr[i] <= k) k++;
-            else break;
+        // for(int i = 0; i<n; i++){
+        //     if(arr[i] <= k) k++;
+        //     else break;
+        // }
+        // return k;
+
+        int s = 0;
+        int e = arr.length - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            // Missing numbers before arr[mid]
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
         }
-        return k;
+
+        return s + k;
     }
 }
