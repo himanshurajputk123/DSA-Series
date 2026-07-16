8a4117634f74a62c4dcd0f61528cc4124a8719cb
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-07-16 13:00:30 +0530

    Time: 0 ms (100%), Space: 44.4 MB (18.26%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -19,7 +19,7 @@ class Solution {
         int s = 1; int e = n - 2;
         while(s <= e){
             int mid = s + (e - s)/2;
-            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+            if( (nums[mid] > nums[mid - 1]) && (nums[mid] > nums[mid + 1]) ) return mid;
             else if(nums[mid] > nums[mid - 1]) s = mid + 1;
             else e = mid - 1;
         }
