eefca4241fd0bc1fa2ab071d62d9abc9198b1870
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-07-20 22:16:28 +0530

    Time: 4 ms (82.28%), Space: 46.4 MB (42.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,21 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        char[] str1 = s.toCharArray();
+    public boolean isBruteForce(String s, String t){
+        // T.C => O(2n) + O(2nlogn) + O(n) ==> O(nlogn)
+        char[] str1 = s.toCharArray(); // toCharArray() takes T.C => O(n)
         char[] str2 = t.toCharArray();
         if(str1.length != str2.length){
             return false;
         }
-        Arrays.sort(str1);
+        Arrays.sort(str1); // sorting T.C => O(nlogn)
         Arrays.sort(str2);
-        for(int i = 0; i<str1.length; i++){
+        for(int i = 0; i<str1.length; i++){ // loop T.C => O(n)
             if(str1[i] != str2[i]){
                 return false;
             }
         }
         return true;
+    }
+    public boolean isAnagram(String s, String t) {
+        return isBruteForce(s, t);
     }
 }
