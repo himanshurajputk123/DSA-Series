880702fcadd57699dca9adccb6e8ecd0513898cb
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-07-20 17:28:52 +0530

    Time: 4 ms (2.94%), Space: 43.6 MB (17.82%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean bruteForce(String s, String goal){
+        // bruteForce is rotate characters of string one by one
+
+        if(s.length() != goal.length()){
+            return false;
+        }
+        String currentString = s;
+
+        for(int i = 0; i<s.length(); i++){
+            // check if s == goals?
+            if(currentString.equals(goal)){
+                return true;
+            }
+            currentString = currentString.substring(1) + currentString.charAt(0);
+            
+        }
+        return false;
+    }
+    public boolean rotateString(String s, String goal) {
+        
+            // both space and time complexity => O(n)
+        // if(s.length() != goal.length()){
+        //     return false;
+        // }
+        // return (s + s).contains(goal); // O(n)
+
+        return bruteForce(s, goal);
+    }
+}
