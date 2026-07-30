e9d113156394cc3fd9bfc82216eab12502ec6383
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-07-30 18:54:02 +0530

    Time: 8 ms (99.71%), Space: 47.9 MB (73.84%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,22 +1,45 @@
 class Solution {
-    public int minimumPushes(String word) {
+    public static int isOptimal(String word){
+    //  use a fixed size array instead of HashMap
         int n = word.length();
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] freq = new int[26];
         for(int i = 0; i<n; i++){
             char ch = word.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            freq[ch - 'a']++;
         }
-        // Step 2. Jiski jada frequency hai use kum push alot krne hain means
-        // sort map in descending order
-        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((a, b) -> b.getValue().compareTo(a.getValue()));
 
+        // sort freq array in descending order
+        // traverse in reverse
+        Arrays.sort(freq);
         int totalCost = 0;
-        for(int i = 0; i<list.size(); i++){
-            int pushCost = (i/8) + 1;
-            totalCost += pushCost*list.get(i).getValue();
+
+        for (int i = 25, idx = 0; i >= 0; i--, idx++) {
+            int pushCost = (idx / 8) + 1;
+            totalCost += pushCost * freq[i];
         }
 
-        return totalCost;
+        return totalCost;    
+    }
+
+    public int minimumPushes(String word) {
+        // int n = word.length();
+        // HashMap<Character, Integer> map = new HashMap<>();
+        
+        // for(int i = 0; i<n; i++){
+        //     char ch = word.charAt(i);
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
+        // // Step 2. Jiski jada frequency hai use kum push alot krne hain means
+        // // sort map in descending order
+        // List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+        // list.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+
+        // for (int i = 25, idx = 0; i >= 0; i--, idx++) {
+        //     totalCost += pushCost*list.get(i).getValue();
+            
+        // }
+
+        // return totalCost;
+        return isOptimal(word);
     }
 }
