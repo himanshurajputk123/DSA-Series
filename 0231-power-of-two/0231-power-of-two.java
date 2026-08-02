09975755b4353b6b89b358c349de18a280527c0d
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-08-02 16:04:08 +0530

    Time: 1 ms (96.62%), Space: 42.6 MB (37.39%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 0 || n < 0) return false;
+        else if((n & (n - 1)) == 0) return true;
+        else return false;
+
+        // return (n & (n - 1)) == 0;
+    }
+}
