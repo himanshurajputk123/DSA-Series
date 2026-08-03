cd166f809d04d25df0e5f6db53f416b4d37f785d
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-08-03 16:53:29 +0530

    Time: 0 ms (100%), Space: 42.5 MB (6.2%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,9 +1,14 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int flip = start ^ goal;
+        // the number of setBits in xor be our answer
+        /*
+        Start = 10; Goal = 7
+        01010 ^ 00111 => 01101 Here The number of setBits is our answer
+        */
+        int xor = start ^ goal;
         int ans = 0;
-        while(flip != 0){
-            flip = flip & (flip - 1);
+        while(xor != 0){
+            xor = xor & (xor - 1);
             ans++;
         }
         return ans;
