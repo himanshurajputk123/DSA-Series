6af07e8cef90f4303a295792b3fd502313d326b0
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-08-26 01:52:52 +0530

    Time: 3 ms (23.65%), Space: 45.7 MB (6.87%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,20 +1,33 @@
 class Solution {
-    public int missingMultiple(int[] nums, int k) {
+    public static int betterSolution(int[] nums, int k){
         HashSet<Integer> set = new HashSet<>();
         int n = nums.length;
-        for(int i = 0; i < n; i++){
-            if(nums[i] % k == 0){
-                set.add(nums[i]);
-            }            
+        for(int num : nums){
+            set.add(num);
         }
-        int max = 0;
-        if(!set.isEmpty()){
-            max = Collections.max(set);
+        for(int i = k; ; i += k){
+            if(!set.contains(i))
+                return i;
         }
-        for(int i = k; i <= max; i += k){
-            if(!set.contains(i)) return i;
-        }
-        return max + k;
+    }
+    public int missingMultiple(int[] nums, int k) {
+        // HashSet<Integer> set = new HashSet<>();
+        // int n = nums.length;
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i] % k == 0){
+        //         set.add(nums[i]);
+        //     }            
+        // }
+        // int max = 0;
+        // if(!set.isEmpty()){
+        //     max = Collections.max(set);
+        // }
+        // for(int i = k; i <= max; i += k){
+        //     if(!set.contains(i)) return i;
+        // }
+        // return max + k;
+
+        return betterSolution(nums, k);
 
         
     }
