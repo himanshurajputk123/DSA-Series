c41e8d040046b17d8d1c6f37cb44ab49dfdacecc
Himanshu Rajput <himanshurajputk123@gmail.com>
2026-09-16 18:52:34 +0530

    Time: 3 ms (53.55%), Space: 45 MB (76.57%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public static void helperFunc(int[] nums, int i, List<List<Integer>> ans, List<Integer> list){
+        if(i == nums.length){
+            List<Integer> temp;
+            temp = list;
+            Collections.sort(temp);
+            if(!ans.contains(temp)){
+                ans.add(new ArrayList<>(temp));
+            }
+            return;
+        }
+        // pick
+        list.add(nums[i]);
+        helperFunc(nums, i+1, ans, list);
+
+
+        // not pick
+        list.remove(list.size()-1);
+        helperFunc(nums, i+1, ans, list);
+    }
+
+    public static void backtracker(int[] nums, int idx, List<List<Integer>> ans, List<Integer> list){
+        ans.add(new ArrayList<>(list));
+        
+        for(int i = idx; i<nums.length; i++){
+            if(i != idx && nums[i] == nums[i-1]) continue;
+            list.add(nums[i]);
+            backtracker(nums, i+1, ans, list);
+            list.removeLast();
+        }
+        
+    }
+    
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        //helperFunc(nums, 0, ans, new ArrayList<>());
+        Arrays.sort(nums);
+        backtracker(nums, 0, ans, new ArrayList<>());
+
+        return ans;
+    }
+}
